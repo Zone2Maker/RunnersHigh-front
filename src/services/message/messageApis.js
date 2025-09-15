5adce0eb4599346a071c665d19c3df1e6f28bb8a
WonYoung <error071312@gmail.com>
2025-09-15 21:28:11 +0900

    refactor: 메시지 API 호출 수정

--- src/services/message/messageApis.js
+++ src/services/message/messageApis.js
@@ -2,9 +2,13 @@ import { instance } from "../services/instance";
 
 export const getMessageListRequest = async (crewId, cursorMessageId, size) => {
   try {
-    const response = await instance.get(
-      `crew/${crewId}/messages?cursorMessageId=${cursorMessageId}&size=${size}`
-    );
+    const response = await instance.get("/crew", {
+      params: {
+        crewId: crewId,
+        cursorMessageId: cursorMessageId,
+        size: size,
+      },
+    });
     return response;
   } catch (error) {
     return error.response;
