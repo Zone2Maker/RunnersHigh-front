036785d0542b1347ec2fecac7934967f195b2b23
WonYoung <error071312@gmail.com>
2025-09-15 20:58:58 +0900

    feat: 백엔드 메시지 API 호출 함수 추가

---
+++ src/services/messageApis/messageApis.js
@@ -0,0 +1,12 @@
+import { instance } from "../services/instance";
+
+export const getMessageListRequest = async (crewId, cursorMessageId, size) => {
+  try {
+    const response = await instance.get(
+      `crew/${crewId}/messages?cursorMessageId?=${cursorMessageId}&size=${size}`
+    );
+    return response;
+  } catch (error) {
+    return error.response;
+  }
+};
