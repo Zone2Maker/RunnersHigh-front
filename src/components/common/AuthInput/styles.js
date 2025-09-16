fdae2ed8cc038525f9d793e0d8c66dde298e57f4
earlyerin <rin050301@gmail.com>
2025-09-16 20:08:15 +0900

    feat: InputBox

--- src/components/common/AuthInput/styles.js
+++ src/components/common/AuthInput/styles.js
@@ -3,18 +3,21 @@ import { css } from "@emotion/react";
 export const input = css`
   width: 100%;
   padding: 18px 16px;
-  margin-bottom: 12px;
+  outline: none;
+  border: none;
 
   font-size: 1rem;
   color: #333;
-  
+
   background-color: #f9f9f9;
-  border: 1px solid #e8e8e8;
-  border-radius: 8px;
+  border-bottom: 1px solid #e8e8e8;
   box-sizing: border-box;
+
+  &:last-of-type {
+    border-bottom: none;
+  }
 
   &::placeholder {
     color: #999;
   }
-
 `;
