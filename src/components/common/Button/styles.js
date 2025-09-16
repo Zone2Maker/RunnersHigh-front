9a4365219c061ebc2296c245e9618e85e389b76f
WonYoung <error071312@gmail.com>
2025-09-16 14:51:31 +0900

    feat: css 수정

---
+++ src/components/common/Button/styles.js
@@ -0,0 +1,13 @@
+import { css } from "@emotion/react";
+
+export const button = css`
+  /* 버튼을 담고있는 div가 flex여야 함 */
+  flex-grow: 1;
+  padding: 12px;
+  border-radius: 10px;
+  border: none;
+  background-color: #5a5a5a;
+  color: #efefef;
+  font-size: 18px;
+  box-sizing: border-box;
+`;
