79a081ddd6e080396863f6220410fb727786d98d
WonYoung <error071312@gmail.com>
2025-09-17 19:47:43 +0900

    �: Header, Layout, BottomNavBar css 수정

--- src/components/layout/Header/styles.js
+++ src/components/layout/Header/styles.js
@@ -3,17 +3,23 @@ import { css } from "@emotion/react";
 export const container = css`
   position: sticky;
   top: 0;
+  flex-shrink: 0;
 
   width: 100%;
-  height: 80px;
+  height: 60px;
+  padding: 0 10px;
   background-color: #fefefe;
   z-index: 10;
 
-  text-align: center;
-  line-height: 80px;
-  font-size: 30px;
-  font-weight: 800;
-  color: #0d47a1;
-
   box-shadow: 0 5px 5px -5px rgba(0, 0, 0, 0.5);
+
+  & > p {
+    padding: 0;
+    margin: 0;
+    text-align: left;
+    line-height: 60px;
+    font-size: 28px;
+    font-weight: 800;
+    color: #0d47a1;
+  }
 `;
--- src/components/layout/Layout/styles.js
+++ src/components/layout/Layout/styles.js
@@ -1,9 +1,16 @@
 import { css } from "@emotion/react";
 
 export const container = css`
-  width: 600px;
+  width: 500px;
   min-height: 100vh;
   margin: 0 auto;
   background-color: #fefefe;
   box-shadow: 0px 0px 7px 3px rgba(207, 207, 207, 0.5);
+  display: flex;
+  flex-direction: column;
+`;
+
+export const mainContainer = css`
+  flex-grow: 1;
+  overflow-y: auto;
 `;
