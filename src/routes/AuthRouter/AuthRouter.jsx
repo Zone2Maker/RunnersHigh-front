1651666bd5378a86ad3bd573be2409e235392e46
jiny-01 <allie7019@naver.com>
2025-09-20 21:15:42 +0900

    임시 커밋: 로컬 수정 저장

--- src/routes/AuthRouter/AuthRouter.jsx
+++ src/routes/AuthRouter/AuthRouter.jsx
@@ -1,7 +1,4 @@
 import { Route, Routes } from "react-router-dom";
-import Login from "../../pages/Auth/Login/Login";
-import oAuth2Merge from "../../pages/Auth/OAuth2Merge/OAuth2Merge";
-import OAuth2Merge from "../../pages/Auth/OAuth2Merge/OAuth2Merge";
 
 function AuthRouter() {
   return (
@@ -9,7 +6,7 @@ function AuthRouter() {
       <Route path="join" />
       <Route path="login" element={<Login />} />
       <Route path="/oauth2/entry" />
-      <Route path="/oauth2/merge" element={<OAuth2Merge />} />
+      <Route path="oauth2/merge" element={<OAuth2Merge />} />
       <Route path="oauth2/join" />
       <Route path="oauth2/redirect" />
     </Routes>
--- src/services/oAuth2/oAuth2Apis.js
+++ src/services/oAuth2/oAuth2Apis.js
@@ -1,4 +1,4 @@
-import { instance } from "../services/instance";
+import { instance } from "../instance/instance";
 
 export const joinOAuth2UserReq = async (data) => {
   try {
