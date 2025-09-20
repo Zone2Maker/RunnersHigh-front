d46543e072f38712c6fa20cb604c64e7e4ec1452
earlyerin <rin050301@gmail.com>
2025-09-20 13:01:39 +0900

    feat: 라우터 경로 수정

--- src/routes/CrewRouter/CrewRouter.jsx
+++ src/routes/CrewRouter/CrewRouter.jsx
@@ -1,11 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import CrewMain from "../../pages/Crew/CrewMain/CrewMain";
+import CrewRegister from "../../pages/Crew/CrewRegister/CrewRegister";
 
 function CrewRouter() {
   return (
     <Routes>
       <Route path="" element={<CrewMain />} />
-      <Route path="/new" />
+      <Route path="new" element={<CrewRegister />} />
     </Routes>
   );
 }
