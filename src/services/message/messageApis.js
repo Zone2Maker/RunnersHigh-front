50ef747910bbc2012ebb8180860822198bb18174
earlyerin <rin050301@gmail.com>
2025-09-26 12:16:27 +0900

    feat: API 요청 추가작성

--- src/services/message/messageApis.js
+++ src/services/message/messageApis.js
@@ -1,13 +1,54 @@
 import { instance } from "../instance/instance";
 
-export const getMessageListReq = async (crewId, cursorMessageId, size) => {
+export const getMessageListReq = async (
+  crewId,
+  prevCursorId,
+  nextCursorId,
+  size,
+  direction
+) => {
   try {
-    const response = await instance.get(`/crew/${crewId}/messages`, {
+    const response = await instance.get(`/crews/${crewId}/messages`, {
       params: {
-        cursorMessageId: cursorMessageId,
+        prevCursorId: prevCursorId,
+        nextCursorId: nextCursorId,
         size: size,
+        direction: direction,
       },
     });
+    return response;
+  } catch (error) {
+    return error.response;
+  }
+};
+
+export const getLastReadMessageIdReq = async (crewId) => {
+  try {
+    const response = await instance.get(`/crews/${crewId}/messages/last-read`);
+    return response;
+  } catch (error) {
+    return error.response;
+  }
+};
+
+export const updateLastReadMessageIdReq = async (crewId, lastReadMessageId) => {
+  try {
+    const response = await instance.post(
+      `/crews/${crewId}/messages/read-update`,
+      {}, // body 비우기
+      { params: { lastReadMessageId } }
+    );
+    return response;
+  } catch (error) {
+    return error.response;
+  }
+};
+
+export const getUnreadMessageCountReq = async (crewId) => {
+  try {
+    const response = await instance.get(
+      `/crews/${crewId}/messages/unread-count`
+    );
     return response;
   } catch (error) {
     return error.response;
