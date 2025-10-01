a5024705718117c57a7b581346be621298f4821c
WonYoung <error071312@gmail.com>
2025-10-01 19:29:04 +0900

    fix: OpenWeatherAPI 요청 주소 수정

--- src/services/weather/weatherApis.js
+++ src/services/weather/weatherApis.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const WEATHER_API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
 const WEATHER_API_BASE_URL = `https://api.openweathermap.org/data/2.5/weather`;
-const AIR_API_BASE_URL = `http://api.openweathermap.org/data/2.5/air_pollution`;
-const GEO_API_BASE_URL = `http://api.openweathermap.org/geo/1.0/reverse`;
+const AIR_API_BASE_URL = `https://api.openweathermap.org/data/2.5/air_pollution`;
+const GEO_API_BASE_URL = `https://api.openweathermap.org/geo/1.0/reverse`;
 
 export const getWeatherReq = async (lat, lng) => {
   const url = `${WEATHER_API_BASE_URL}?lat=${lat}&lon=${lng}&appid=${WEATHER_API_KEY}&lang=kr&units=metric`;
